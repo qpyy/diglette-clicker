80091a880293c01d9ba9b0af1bcaffe90ee46405
Qpyy <boom8008@yandex.ru>
2024-08-11 22:11:43 +0300

    fix: backend edit cors

--- backend/api/server.js
+++ backend/api/server.js
@@ -1,20 +1,20 @@
 const fastify = require("fastify")();
-const fastifyPostgres = require('@fastify/postgres');
-const { router } = require('./routes/routes.js')
-const cookiePlugin = require('fastify-cookie');
-const authMiddleware = require('./middleware/auth-middleware.js');
-require('dotenv').config();
+const fastifyPostgres = require("@fastify/postgres");
+const { router } = require("./routes/routes.js");
+const cookiePlugin = require("fastify-cookie");
+const authMiddleware = require("./middleware/auth-middleware.js");
+require("dotenv").config();
 
 fastify.register(cookiePlugin, {
   secret: process.env.COOKIE_SECRET, // Секретный ключ для подписи cookie
-  parseOptions: {}    // Дополнительные параметры для парсинга cookie
+  parseOptions: {}, // Дополнительные параметры для парсинга cookie
 });
 
 fastify.register(require("@fastify/cors"), {
   // Настройки CORS
   origin: "*",
   methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["content-type"],
+  allowedHeaders: ["content-type", "Authorization"],
 });
 
 const connectServer = async () => {
@@ -26,21 +26,27 @@ const connectServer = async () => {
 connectServer();
 
 // Не защищённые маршруты
-fastify.register((instance, opts, done) => {
-  instance.route(router().registration);
-  instance.route(router().authorization);
-  instance.route(router().refresh);
-  done();
-}, { prefix: '/' });
+fastify.register(
+  (instance, opts, done) => {
+    instance.route(router().registration);
+    instance.route(router().authorization);
+    instance.route(router().refresh);
+    done();
+  },
+  { prefix: "/" }
+);
 
 // Защищённые маршруты
-fastify.register((instance, opts, done) => {
-  instance.addHook('preHandler', authMiddleware);
-  instance.route(router().takeCoin);
-  instance.route(router().activate);
-  instance.route(router().logout);
-  done();
-}, { prefix: '/' });
+fastify.register(
+  (instance, opts, done) => {
+    instance.addHook("preHandler", authMiddleware);
+    instance.route(router().takeCoin);
+    instance.route(router().activate);
+    instance.route(router().logout);
+    done();
+  },
+  { prefix: "/" }
+);
 
 fastify.listen({ port: 4000, host: "0.0.0.0" }, (err) => {
   if (err) {
